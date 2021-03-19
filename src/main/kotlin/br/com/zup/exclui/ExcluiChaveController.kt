1c3d3e8c7188729be103a2d9b24d4901c9281975
Marcelo Felix <celofelix93@gmail.com>
2021-03-19 15:36:53 -0300

    Criado o controller para remoção pelo Id da chave e id do cliente

---
+++ src/main/kotlin/br/com/zup/exclui/ExcluiChaveController.kt
@@ -0,0 +1,33 @@
+package br.com.zup.exclui
+
+import br.com.zup.ExcluiChavePixRequest
+import br.com.zup.KeyManagerExcluiGrpcServiceGrpc
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Delete
+import io.micronaut.http.annotation.PathVariable
+import io.micronaut.validation.Validated
+import java.util.*
+import javax.inject.Inject
+
+@Validated
+@Controller("/deleta/pix/{pixId}")
+class ExcluiChaveController(
+    @Inject val excluiChaveClient:
+    KeyManagerExcluiGrpcServiceGrpc.KeyManagerExcluiGrpcServiceBlockingStub
+) {
+
+    @Delete("/cliente/{clienteId}")
+    fun deleta(@PathVariable pixId: Long, @PathVariable clienteId: UUID): HttpResponse<Any> {
+
+        val excluiChaveRequest = ExcluiChavePixRequest.newBuilder()
+            .setPixId(pixId.toString())
+            .setClienteId(clienteId.toString())
+            .build()
+
+        excluiChaveClient.exclui(excluiChaveRequest)
+
+        return HttpResponse.ok()
+    }
+
+}
