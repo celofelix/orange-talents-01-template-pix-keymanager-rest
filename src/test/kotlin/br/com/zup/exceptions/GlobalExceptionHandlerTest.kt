5f26eae9c2fd387f55d9299339e32e85350b5027
Marcelo Felix <celofelix93@gmail.com>
2021-04-13 15:38:05 -0300

    Criados os testes de unidade para a GlobalExceptionHandler verificando os status de 400 e 422

--- src/test/kotlin/br/com/zup/exceptions/GlobalExceptionHandlerTest.kt
+++ src/test/kotlin/br/com/zup/exceptions/GlobalExceptionHandlerTest.kt
@@ -13,7 +13,7 @@ class GlobalExceptionHandlerTest {
     val request = HttpRequest.GET<Any>("/")
 
     @Test
-    fun `deve lancar 404 para statusException not found`() {
+    fun `deve lancar 404 para StatusRuntimeException not found`() {
 
         val mensagem = "Não foi encontrado com os dados informado"
         val notFoundException = StatusRuntimeException(Status.NOT_FOUND.withDescription(mensagem))
@@ -22,6 +22,36 @@ class GlobalExceptionHandlerTest {
 
         with(response) {
             Assertions.assertEquals(HttpStatus.NOT_FOUND, status)
+            Assertions.assertNotNull(body())
+            Assertions.assertEquals(mensagem, (body() as JsonError).message)
+        }
+    }
+
+    @Test
+    fun `deve lancar 400 para StatusRuntimeException Invalid Argument`() {
+
+        val mensagem = "Dados preenchidos inválidos"
+        val invalidArgument = StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription(mensagem))
+
+        val response = GlobalExceptionHandler().handle(request, invalidArgument)
+
+        with(response) {
+            Assertions.assertEquals(HttpStatus.BAD_REQUEST, status)
+            Assertions.assertNotNull(body())
+            Assertions.assertEquals(mensagem, (body() as JsonError).message)
+        }
+    }
+
+    @Test
+    fun `deve lancar 422 para StatusRuntimeException Already Exists`() {
+
+        val mensagem = "Dados informados já existem"
+        val alreadyExists = StatusRuntimeException(Status.ALREADY_EXISTS.withDescription(mensagem))
+
+        val response = GlobalExceptionHandler().handle(request, alreadyExists)
+
+        with(response) {
+            Assertions.assertEquals(HttpStatus.UNPROCESSABLE_ENTITY, status)
             Assertions.assertNotNull(body())
             Assertions.assertEquals(mensagem, (body() as JsonError).message)
         }
